8c9eecbac04b4582208f4def89fa57f4450814c5
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2023-10-06 17:32:19 +0300

    6.10.2023

--- btk-makine/nb.py
+++ btk-makine/nb.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import confusion_matrix
 
 data = pd.read_csv("data/veriler.csv")
 print(data)
@@ -15,43 +16,41 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.33, random_
 
 #verilerin ölçeklendirilmesi
 from sklearn.preprocessing import StandardScaler
-
 sc = StandardScaler()
-
 X_train = sc.fit_transform(x_train) #  fit:eğitme transform: uygulama
 X_test = sc.transform(x_test)
 
-# obje oluşturuyoruz
+# Sınıflandırma Algoritmaları...
+
+#LogisticRegression
 from sklearn.linear_model import LogisticRegression
-
 logr = LogisticRegression(random_state=0)
-logr.fit(X_train, y_train)
+logr.fit(X_train, y_train) #egitim
 
-y_pred = logr.predict(X_test)
+y_pred = logr.predict(X_test) #tahmin
 print(y_pred)
 print(y_test)
-
-# tahmin ile asıl veri doğruluk oranı sonucunu veriyor
-from sklearn.metrics import confusion_matrix
+# verimiz ile tahmin verisini kontrol ediyor ve hata oranını söylüyor(karmaşıklık matrisi)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 
 #KNN algoritması
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=1, metric='minkowski')
-knn.fit(X_train, y_train)
+knn.fit(X_train, y_train) #eğitim
 
-y_pred = knn.predict(X_test)
-
+y_pred = knn.predict(X_test) #tahmin
+#karmaşıklık matrisi
 cm= confusion_matrix(y_test, y_pred)
 print(cm)
 
 # SVC
 from sklearn.svm import SVC
 svc = SVC(kernel='rbf')
-svc.fit(X_train, y_train)
+svc.fit(X_train, y_train) # eğitim 
 
-y_pred = svc.predict(X_test)
+y_pred = svc.predict(X_test) # tahmin
+# karmaşıklık matrisi ile test veri ile kıyaslama 
 cm = confusion_matrix(y_test, y_pred)
 print("SVC")
 print(cm)
@@ -59,9 +58,30 @@ print(cm)
 #naif bayes
 from sklearn.naive_bayes import GaussianNB
 gnb = GaussianNB()
-gnb.fit(X_train, y_train)
+gnb.fit(X_train, y_train) #eğitim
 
-y_pred = gnb.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
+y_pred = gnb.predict(X_test) #tahmin 
+cm = confusion_matrix(y_test, y_pred) #sonuç test
 print('GNB')
 print(cm)
+
+# DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+dtc = DecisionTreeClassifier(criterion='entropy')
+dtc.fit(X_train, y_train) # eğitim
+y_pred = dtc.predict(X_test) # tahmin
+
+cm = confusion_matrix(y_test, y_pred) #test
+print('DTC')
+print(cm)
+
+#RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators=10, criterion='entropy')
+rfc.fit(X_train, y_train) #eğitim
+y_pred = rfc.predict(X_test) #tahmin
+cm = confusion_matrix(y_test, y_pred) #test
+print('RFC')
+print(cm)
+
+

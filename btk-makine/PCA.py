10d862c444aadbe1a7a0a2da5c93ab25c3476b52
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2023-11-08 17:28:23 +0300

    8.11.2023

---
+++ btk-makine/PCA.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#veri kümesi
+veriler = pd.read_csv("btk-makine/data/Wine.csv")
+X = veriler.iloc[:,0:13].values
+y = veriler.iloc[:,13].values
+
+#eğitim ve test kümelerinin bölünmesi
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
+
+#Ölçekleme
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+X_train2 = pca.fit_transform(X_train)
+X_test2 = pca.fit_transform(X_test)
+
+#pca dönüşümünden önce gelen LR
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+#pca dönüşümünden sonra gelen LR
+classifier2 = LogisticRegression(random_state=0)
+classifier2.fit(X_train2, y_train)
+
+#tahminler
+y_pred = classifier.predict(X_test)
+
+y_pred2 = classifier2.predict(X_test2)
+
+
+#actual / PCA olmadan çıkan sonuç
+from sklearn.metrics import confusion_matrix
+print("Gerçek PCA'sız: ")
+cm = confusion_matrix(y_test, y_pred)
+
+print(cm)
+
+#actual / PCA sonrası çıkan sonuç
+print(" \n\nGerçek PCA ile: ")
+cm2 = confusion_matrix(y_test, y_pred2)
+print(cm2)
+
+#PCA sonrası / PCA öncesi
+print("\n\nPCA'lı ve PCA'sız çıkan sonuç: ")
+cm3 = confusion_matrix(y_pred, y_pred2)
+print(cm3)
+
--- btk-makine/ann-verionisleme.py
+++ btk-makine/ann-verionisleme.py
@@ -46,3 +46,12 @@ classifier.add(Dense(6, kernel_initializer = "uniform", activation= "relu", inpu
 classifier.add(Dense(6, kernel_initializer="uniform", activation="relu"))
 classifier.add(Dense(1, kernel_initializer="uniform", activation="sigmoid"))
 classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics= ['accuracy'])
+classifier.fit(X_train, y_train, epochs=50)
+
+y_pred = classifier.predict(X_test)
+
+y_pred = (y_pred > 0.5) 
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
